4f04e9eff15b8ea5a141ad0caaec7ee5f1775aed
raziyeh sadat Hashemi <raziyeh303@gmail.com>
2025-06-04 16:51:44 +0330

    change directory

--- src/User/decorator/roles.drcorator.ts
+++ src/User/decorator/roles.drcorator.ts
@@ -1,4 +1,4 @@
 import { SetMetadata } from '@nestjs/common';
-import { UserRole } from '../enum/role.enum';
+import { UserRole } from '../model/enum/role.enum';
 
 export const Roles = (role: UserRole) => SetMetadata('role', role);
